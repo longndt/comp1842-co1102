7491c5755685864cd8c36d246393cf16d678b897
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-07-17 17:07:57 +0700

    demo

--- vocab-builder/server/server.js
+++ vocab-builder/server/server.js
@@ -1,44 +1,20 @@
-//declare & import 'express' library
 const express = require('express')
-
-//delare "fs" (file system) library
-const fs = require('fs')
-
-//declare server port
 const port = 3000
-
-//declare "app" server as "express" instance
 const app = express()
 
-//run server by listening to port
+//declare & config "mongoose"
+const mongoose = require('mongoose')
+const db = "mongodb://localhost:27017/vocab-builder"
+//vocab-builder: database name
+mongoose.connect(db)
+   .then(() => console.log('ok'))
+.catch((err) => console.error(err))
+
+//declare & config "body-parser"
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.listen(port)
-
-//display homepage
-app.get("/", (req, res) => {
-   res.setHeader('Content-Type', 'text/html')
-   fs.readFile('views/index.html', (err, data) => {
-      if (err) {
-         console.error ('Read file error !')
-      } else {
-         res.write(data)
-      }
-      res.end()
-   })
-})
-
-//display login page
-app.get("/login", (req, res) => {
-   res.setHeader('Content-Type', 'text/html')
-   res.write("This is login page")
-   res.end()
-})
-
-//display welcome page
-app.get("/welcome", (req, res) => {
-   res.send("<h1>Welcome to my web app</h1>")
-})
-
-//console.log('Server is running at http://localhost:' + port)
-console.log(`Server is running at http://localhost:${port}`)
 
 //Run web app in terminal : npm start
