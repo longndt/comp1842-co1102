bf1ffba42a6e1b90abd3ba0a543ee7674249e813
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-07-24 17:30:54 +0700

    client

--- vocab-builder/client/src/helpers/helpers.js
+++ vocab-builder/client/src/helpers/helpers.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000/words/'
+const baseURL = 'http://localhost:3000/vocabs/'
 
 const handleError = fn => (...params) =>
 fn(...params).catch(error => {
