19084ad528a2e33b800a018810a4924181b87d24
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-07-24 16:24:49 +0700

    express

--- express/web2/app.js
+++ express/web2/app.js
@@ -11,6 +11,10 @@ var demoRouter = require('./routes/demo');
 
 var app = express();
 
+// declare, import & config 'body-parser' library
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded( { extended : true }))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -26,13 +30,13 @@ app.use('/users', usersRouter);
 //set link (url) for router (2)
 app.use('/demo', demoRouter)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// return a custom error for 404 - Page Not Found
+app.use((req, res) => {
+  res.status(404).send("url: " + req.originalUrl + " not found !")
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,5 +45,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- express/web2/routes/index.js
+++ express/web2/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index');
 });
 
@@ -15,5 +15,60 @@ router.get('/fpt', function (req, res) {
 router.get('/hello', function (req, res, next) {
   res.send('say hello from index router');
 });
+
+//localhost:3000/data
+router.get('/data', (req, res) => {
+  var country = "Vietnam"
+  var year = 2024
+  var greenwich = "Greenwich Vietnam"
+  //views/data.hbs
+  res.render('data',
+    {
+      country,
+      year,
+      fgw: greenwich,
+      comp1842: "Web Programming 2"
+    }
+  )
+})
+
+router.get('/sports', (req, res) => {
+  //create fake data in json format
+  var sports = [
+    {
+      "id" : 1,
+      "english": "badminton",
+      "vietnamese" : "cầu lông"
+    },
+    {
+      "id": 2,
+      "english": "football",
+      "vietnamese": "bóng đá"
+    },
+    {
+      "id": 3,
+      "english": "swimming",
+      "vietnamese": "bơi lội"
+    }
+  ]
+  res.render('sports', { sports })
+})
+
+//render login form
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+//process login form
+router.post('/login', (req, res) => {
+  //get user's input from login form
+  var username = req.body.username
+  var password = req.body.password
+  //var data = "Username : " + username "<br /> Password : " + password
+  if (username == "admin" && password == "123456")
+    res.send("<h1>Login succeed !</h1>")
+  else
+    res.send("<h1>Login failed !</h1>")
+})
 
 module.exports = router;
