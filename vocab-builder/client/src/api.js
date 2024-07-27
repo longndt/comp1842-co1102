9ad5a9ea41a60bbda21487d383e0065c7fcc6416
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-07-27 12:33:25 +0700

    vue

--- vocab-builder/client/src/api.js
+++ vocab-builder/client/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const backend = 'http://localhost:3000/vocabs';
+const backend = 'http://localhost:3000/vocabs'
 
 const getAllWords = async () => {
    try {
-      const response = await axios.get(backend);
-      return response.data;
+      const response = await axios.get(backend)
+      return response.data
    } catch (err) {
       console.error(err)
    }
