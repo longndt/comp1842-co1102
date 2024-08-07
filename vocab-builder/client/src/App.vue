4773ea279e752102d2fe496b1057580ecba1ac77
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-08-07 13:38:15 +0700

    demo

--- vocab-builder/client/src/App.vue
+++ vocab-builder/client/src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    <router-view></router-view>
+    <div class="ui inverted segment navbar">
+      <div class="ui center aligned container">
+        <div class="ui large secondary inverted pointing menu compact">
+          <router-link to="/words" class="item">
+            <i class="list icon"></i>Words
+          </router-link>
+          <router-link to="/words/new" class="item">
+            <i class="plus icon"></i>New
+          </router-link>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui container">
+      <div class="ui grid">
+        <div class="row">
+          <div class="twelve wide column">
+            <router-view />
+          </div>
+          <div class="four wide column">
+            <flash-message></flash-message>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- vocab-builder/client/src/router.js
+++ vocab-builder/client/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Words from './views/Words.vue'
 import New from './views/New.vue'
+import Show from './views/Show.vue'
+import Edit from './views/Edit.vue'
 
 Vue.use(Router)
 
@@ -14,12 +16,24 @@ const router = new Router({
       },
       {
          path: '/words',
+         name: 'Words',
          component: Words
       },
       {
          path: '/words/new',
+         name: 'New',
          component: New
-      }
+      },
+      {
+         path: '/words/show/:id',
+         name: 'Show',
+         component: Show
+      },
+      {
+         path: '/words/edit/:id',
+         name: 'Edit',
+         component: Edit
+      },
    ]
 })
 
